20376475e8313986f9396ee2164a6bd8a7108fa2
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2024-02-19 19:01:54 +1100

    new benchmarks

---
+++ generate_delay2.py
@@ -0,0 +1,84 @@
+import random
+import json
+
+def generate_delay(p):
+    #map_name = 'random'
+    number_agents = 5000
+    simulation_time = 1000
+    #p = 0.2
+    file_name = 'lifelong_benchmark/delay/'+'delay-'+str(p)+'-1.txt'
+
+    delay = []
+
+    for i in range(0,number_agents):
+        agent_delay = []
+        for j in range(0,simulation_time):
+            dp = random.random()
+            if dp > p:
+                agent_delay.append(str(0))
+            else:
+                agent_delay+=[str(1)]*(random.randint(1,5))
+        delay.append(agent_delay)
+
+    with open(file_name,'w') as f:
+        f.write(str(number_agents) + '\n')
+        for a in delay:
+            #print(a)
+            print(len(a[0:1000]))
+            f.write(','.join(a[0:1000]) + '\n')
+        f.close()
+
+
+
+generate_delay(0.01)
+generate_delay(0.02)
+generate_delay(0.03)
+generate_delay(0.04)
+generate_delay(0.05)
+agent_group = [100,200,300,400]
+for agent in agent_group:
+    temp = {'mapFile':'maps/random-32-32-20.map',
+            'agentFile':'agents/agent-'+str(agent)+'_scen-1.agents',
+            'teamSize':agent,
+            'taskFile':'tasks/agent-'+str(agent)+'_scen-1.tasks',
+            'numTasksReveal':1,
+            'taskAssignmentStrategy':'roundrobin',
+            "delayFile":"../delay/delay-0.01-1.txt"}
+    with open('lifelong_benchmark/random/agent-'+str(agent)+'_scen-delay-0.01-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.02-1.txt"
+    with open('lifelong_benchmark/random/agent-'+str(agent)+'_scen-delay-0.02-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.03-1.txt"
+    with open('lifelong_benchmark/random/agent-'+str(agent)+'_scen-delay-0.03-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.04-1.txt"
+    with open('lifelong_benchmark/random/agent-'+str(agent)+'_scen-delay-0.04-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.05-1.txt"
+    with open('lifelong_benchmark/random/agent-'+str(agent)+'_scen-delay-0.05-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+
+agent_group = [200,400,600,800,1000]
+for agent in agent_group:
+    temp = {'mapFile':'maps/warehouse-10-20-10-2-1.map',
+            'agentFile':'agents/agent-'+str(agent)+'_scen-1.agents',
+            'teamSize':agent,
+            'taskFile':'tasks/agent-'+str(agent)+'_scen-1.tasks',
+            'numTasksReveal':1,
+            'taskAssignmentStrategy':'roundrobin',
+            "delayFile":"../delay/delay-0.01-1.txt"}
+    with open('lifelong_benchmark/warehouse-s/agent-'+str(agent)+'_scen-delay-0.01-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.02-1.txt"
+    with open('lifelong_benchmark/warehouse-s/agent-'+str(agent)+'_scen-delay-0.02-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.03-1.txt"
+    with open('lifelong_benchmark/warehouse-s/agent-'+str(agent)+'_scen-delay-0.03-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.04-1.txt"
+    with open('lifelong_benchmark/warehouse-s/agent-'+str(agent)+'_scen-delay-0.04-1.json','w') as f:
+        json.dump(temp,f,indent=2)
+    temp["delayFile"] = "../delay/delay-0.05-1.txt"
+    with open('lifelong_benchmark/warehouse-s/agent-'+str(agent)+'_scen-delay-0.05-1.json','w') as f:
+        json.dump(temp,f,indent=2)
